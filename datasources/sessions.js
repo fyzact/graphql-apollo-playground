2dc0aae073960f62b04b3b49dbbd88b3498ef64d
fyzact <feyyazacet@gmail.com>
2021-05-28 13:13:53 +0100

    add lodash package for filtering

--- datasources/sessions.js
+++ datasources/sessions.js
@@ -1,6 +1,6 @@
 const { DataSource } = require("apollo-datasource");
 const data=require("../data/sessions.json")
-
+_=require("lodash")
 class SessionApi extends DataSource{
     constructor(){
         super()
@@ -13,10 +13,11 @@ class SessionApi extends DataSource{
         return data;
     }
     getSessionById(id){
-        /*
-        you can use lodash
-        */
-      return  data.filter(p=>p.id==parseInt(id))[0];
+    
+    //   return  data.filter(p=>p.id==parseInt(id))[0];
+    //wiht lodash
+    const sessions=_.filter(data,{id:parseInt(id)});
+    return sessions[0];
     }
 }
 
